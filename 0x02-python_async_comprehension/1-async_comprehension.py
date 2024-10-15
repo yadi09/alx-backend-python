13c41bf16c191c1d25ec641d47edd73527bfabe6
Yadamzer <yadamzerterefe09@gmail.com>
2024-10-16 01:43:03 +0300

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """async_comprehension"""
-
-async_generator = __import__("0-async_generator").async_generator
 from typing import List
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
