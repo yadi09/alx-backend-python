2249ebf93d0b3befc7594d535e8a011cde4acebb
Yadamzer <yadamzerterefe09@gmail.com>
2024-10-16 02:09:15 +0300

    Task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""measure runtime"""
+
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+async def measure_runtime():
+    """measure runtime"""
+    list = [
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    ]
+
+    start_time = time.time()
+    
+    await asyncio.gather(*list)
+
+    end_time = time.time()
+    runtime = end_time - start_time
+
+    return runtime
